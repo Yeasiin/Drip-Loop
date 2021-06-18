7d157536cd23916de27ef0f1aa0bde83e27b9e3a
unknown <yeasinarfatemon12345@gmail.com>
2021-06-18 11:19:21 +0600

    showing item in CartBox

--- src/components/CartDropDown.jsx
+++ src/components/CartDropDown.jsx
@@ -1,11 +1,21 @@
+import CartItem from "./CartItem";
+import { connect } from "react-redux";
 import { StyledCartDropDown } from "./../styled/StyledCartDropDown";
 import Button from "./common/Button";
 
-export default function CartDropDown() {
+function CartDropDown({ cartItems }) {
   return (
     <StyledCartDropDown>
-      <div className="cart-items">ok </div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
       <Button value="GO TO CHECKOUT" />
     </StyledCartDropDown>
   );
 }
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+
+export default connect(mapStateToProps)(CartDropDown);
